71064e74df1ae0510694bc7862d12732002e526d
nagrapoonam <nagra.poonam1@gmail.com>
2023-06-17 13:33:24 -0400

    changed the code for template rendering

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-from flask import Flask
-
-app = Flask(__name__)
-
+from flask import render_template
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-
-if __name__ == '__main__':
-    app.run()
+def index():
+    # add code here to fetch the job posts from a file
+    return render_template('index.html', posts=blog_posts)
